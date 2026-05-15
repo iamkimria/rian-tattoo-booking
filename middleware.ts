3e9731dfbf36848306379d07b471429655a83822
Rian <iamkimria@gmail.com>
2026-05-15 23:52:36 +0900

    protect admin route with password

---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    const basicAuth = request.headers.get("authorization");
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(" ")[1];
+      const [user, password] = atob(authValue).split(":");
+
+      if (
+        user === "admin" &&
+        password === process.env.ADMIN_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse("Authentication required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
